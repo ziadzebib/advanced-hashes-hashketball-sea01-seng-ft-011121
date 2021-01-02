56e3f32e70847a1c0f8c2d9d0c4be1c216a6e618
Ziad Zebib <zebibziad@gmail.com>
2021-01-02 05:52:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -158,9 +158,9 @@ end
 def player_numbers(team_name)
   numbers_array = []
   if game_hash[:home][:team_name] == team_names
-    game_hash[:home][:players][:number].each { |key, value| numbers_array << value } 
+    game_hash[:home][:players].each { |key, value| numbers_array << value } 
   elsif game_hash[:away][:team_name] == team_names
-    game_hash[:away][:players][:number].each { |key, value| numbers_array << value } 
+    game_hash[:away][:players].each { |key, value| numbers_array << value } 
   end
   return numbers_array
 end
