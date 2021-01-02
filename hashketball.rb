17f63ef872e3a079a065bd5fbd771d156e9502d2
Ziad Zebib <zebibziad@gmail.com>
2021-01-02 04:52:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -143,9 +143,8 @@ def shoe_size(player_name)
 end
 require 'pry'
 def team_colors(team_names)
-  color_hash = game_hash[:home][:team_name][:team_name] == team_names
-  if 
-    color_hash = game_hash[:away].find{ |test_name| test_name[:team_name] == team_names }
+  if game_hash[:home][:team_name] == team_names
+    return game_hash[:home][:colors]  
   end
   binding.pry
   color_hash[:colors]
