4810e08c10b3ed4c6b4590b1fb75c4fc459f8870
Ziad Zebib <zebibziad@gmail.com>
2021-01-02 03:44:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,7 +128,7 @@ end
 
 def num_points_scored(player_name) 
   points_per_name = 1000
-  game_hash[:home][:players][player].find do |name, points|
+  game_hash[:home][:players].find(player_name)
     name.each do 
   end  
 end
